66458abf0f1adb1fa1b946bf2cdca3c07d96b2f1
Andre Midea <andre.midea@nubank.com.br>
2016-03-03 20:50:41 -0300

    xscreensaver

--- utils-packages.nix
+++ utils-packages.nix
@@ -1,6 +1,18 @@
-{ config, pkgs, ... }:
+{ stdenv, fetchurl, config, pkgs, ... }:
 
-{
+  stdenv.mkDerivation {
+	name = "slack-2.0.0";
+	
+ 	src = fetchurl {
+		url = "https://slack-ssb-updates.global.ssl.fastly.net/linux_releases/slack-desktop-2.0.0-amd64.deb";
+		sha256 = "0ssi1wpaf7plaswqqjwigppsg5fyh99vdlb9kzl7c9lng89ndq1i";
+	};
+	doCheck = false;
+  }
+
+  {
+
+
   environment.systemPackages =
     with pkgs; [
     htop
@@ -17,10 +29,26 @@
     xlsfonts
     ctags
     xorg.xbacklight
+    xscreensaver
 
     # networking
     mtr
     ldns
     ];
+
+
+  systemd.user.services.xscreensaver = {
+	  description = "Xscreen saver";
+
+	  serviceConfig = {
+		  Type = "simple";
+		  ExecStart = "${pkgs.xscreensaver}/bin/xscreensaver";
+		  ExecStop = "${pkgs.xscreensaver}/bin/xscreensaver-command -deactivate";
+		  Restart = "always";
+	  };
+	  wantedBy = [ "default.target" ];
+  };
+
+ systemd.services.xscreensaver.enable = true;
 }
 
